15f362ed2a888b9115402688014400aad47dd316
PranavKaruanan <pranavkaruanant@gmail.com>
2023-02-04 03:29:06 +0530

    almost completed

--- api/controllers/speech.js
+++ api/controllers/speech.js
@@ -1,60 +1,33 @@
 const WordPOS = require("wordpos"),
   wordpos = new WordPOS();
-const multer = require('multer')
-const fs = require('fs')
-
-const upload = multer()
-
-// const { of, from } = require("rxjs");
-// const { map, mergeMap, toArray } = require("rxjs/operators");
 
 module.exports = {
   getSpeech: async (req, res) => {
-    const text = req.file.buffer.toString()
-    // const buffer = fs.readFileSync(req.file.buffer)
-    // console.log(buffer.toString())
-
-    // const text = file.toString();
-    // console.log("text",text)
+    const text = req.file.buffer.toString();
 
     try {
-      console.log("start........................");
-
       const totalWords = text
         .split(/[.!?]/)
         .map((sentence) => sentence.split(" "))
         .flat();
 
-      // const totalWords = sentences.reduce((words, sentence) => words.concat(sentence.split(" ")), []);
-
-      const totalLength = totalWords.length;
-      console.log(totalLength);
-
       const nouns = await wordpos.getNouns(totalWords);
-      console.log(nouns);
-
       const nounsLength = nouns.length;
       const verbs = await wordpos.getVerbs(totalWords);
-      console.log(verbs);
-
       const verbsLength = verbs.length;
       const adjectives = await wordpos.getAdjectives(totalWords);
-      console.log(adjectives);
-
       const adjectivesLength = adjectives.length;
       const adverbs = await wordpos.getAdverbs(totalWords);
-      console.log(adverbs);
       const adverbsLength = adverbs.length;
+      const totalLength =
+        adjectivesLength + verbsLength + nounsLength + adverbsLength;
 
       const dataSet = {
-        nounsPercentage: (nouns.length / totalWords.length) * 100,
-        verbsPercentage: (verbs.length / totalWords.length) * 100,
-        adjectivesPercentage: (adjectives.length / totalWords.length) * 100,
-        adverbsPercentage: (adverbs.length / totalWords.length) * 100,
+        nounsPercentage: (nouns.length / totalLength) * 100,
+        verbsPercentage: (verbs.length / totalLength) * 100,
+        adjectivesPercentage: (adjectives.length / totalLength) * 100,
+        adverbsPercentage: (adverbs.length / totalLength) * 100,
       };
-
-  
-      console.log(dataSet);
       res.json(dataSet);
     } catch (error) {
       console.log(error);
--- api/index.js
+++ api/index.js
@@ -1,23 +1,14 @@
 const express = require ("express")
 const bodyParser = require('body-parser')
 const cors = require ("cors")
-// import cors from 'cors'
-// import mongoose from 'mongoose'
 const speechRouter =require('./routes/speech.js')
-// import speechRouter from './routes/speech.js'
 const dotenv = require('dotenv')
-// import dotenv from 'dotenv'
-
 
 const app = express()
 dotenv.config()
 
-
-
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-
-
 
 app.use(express())
 
