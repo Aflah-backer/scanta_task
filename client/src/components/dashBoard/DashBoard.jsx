fcc37726e6b9867265132979b034615cbe91e9b5
PranavKaruanan <pranavkaruanant@gmail.com>
2023-02-04 03:28:23 +0530

    almost completed

--- client/src/components/dashBoard/DashBoard.jsx
+++ client/src/components/dashBoard/DashBoard.jsx
@@ -1,43 +1,81 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
+import PulseLoader from "react-spinners/PulseLoader";
 import "react-circular-progressbar/dist/styles.css";
 import "./style.css";
 
-function DashBoard() {
-  const nouns = 10;
-  const verbs = 70;
-  const adjectves = 50;
-  const adverbs = 40;
+function DashBoard({ response}) {
+  const [persentage, setPersentage] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (response.data) {
+      setLoading(false);
+    }
+    setPersentage(response.data);
+  }, [response.data]);
+
+  const nouns = Math.floor(persentage?.nounsPercentage);
+  const verbs = Math.floor(persentage?.verbsPercentage);
+  const adjectves = Math.floor(persentage?.adjectivesPercentage);
+  const adverbs = Math.floor(persentage?.adverbsPercentage);
+
+  if(loading){
+    return(
+        <div className="load">
+          <PulseLoader />
+        </div>
+    )
+  }
 
   return (
-    <div className="dashBoardContainer">
-      <div className="wrapper">
-        <div className="box">
-          <div className="card">
-            <CircularProgressbar  value={nouns} text={`${nouns}%`} />
-            <div className="text"><span>NOUN</span></div>
+    // <>
+    <div className="dashboardWrapper">
+        <div className="dashBoardContainer">
+          <div className="wrapper">
+            <div className="box">
+              <div className="card">
+                <CircularProgressbar  value={nouns} text={`${nouns}%`} />
+                <div className="text">
+                  <span>NOUN</span>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="card">
+                <CircularProgressbar value={verbs} text={`${verbs}%`} />
+                <div className="text">
+                  <span>VERB</span>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="card">
+                <CircularProgressbar value={adjectves} text={`${adjectves}%`} />
+                <div className="text">
+                  <span>ADJECTIVE</span>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="card">
+                <CircularProgressbar value={adverbs} text={`${adverbs}%`} />
+                <div className="text">
+                  <span>ADVERB</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="box">
-          <div className="card">
-            <CircularProgressbar value={verbs} text={`${verbs}%`} />
-            <div className="text"><span>VERB</span></div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="card">
-            <CircularProgressbar value={adjectves} text={`${adjectves}%`} />
-            <div className="text"><span>ADJECTIVE</span></div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="card">
-            <CircularProgressbar value={adverbs} text={`${adverbs}%`} />
-            <div className="text"><span>ADVERB</span></div>
-          </div>
-        </div>
+      <div className="btnForUploadNew">
+          <button className="uploadNew" onClick={() => {
+                   
+                  }}>upload new File</button>
       </div>
     </div>
+    // </>
   );
 }
 
--- client/src/components/fileUploader/FileUpload.jsx
+++ client/src/components/fileUploader/FileUpload.jsx
@@ -4,81 +4,85 @@ import "./style.css";
 import { MdCloudUpload, MdDelete } from "react-icons/md";
 import { AiFillFileImage } from "react-icons/ai";
 import axios from "axios";
+import DashBoard from "../dashBoard/DashBoard";
 
 export default function FileUpload() {
   const [file, setFile] = useState("");
   const [fileName, setFileName] = useState("No selected file");
-  
+  const [res, setRes] = useState({});
+  const [loading,setLoading] =useState(false)
+
   const handleFileChange = (event) => {
-  
     setFile(event.target.files[0]);
     setFileName(event.target.files[0].name);
   };
 
-
-
   const submitData = async (e) => {
     e.preventDefault();
+    setLoading(true)
     const formData = new FormData();
-    formData.append("file", file)
+    formData.append("file", file);
     const responce = await axios.post(
-        "http://localhost:5000/speech/addSpeech",
-        formData
-      );
-     
-    console.log(responce)
+      "http://localhost:5000/speech/addSpeech",
+      formData
+    );
+    setRes(responce);
   };
-
- 
-
-  // console.log("fileBuffer", fileBuffer);
-
+  console.log(res)
   return (
-    <main>
-      <div className="wrapper">
-        <form onClick={() => document.querySelector(".input-field").click()}>
-          <input
-            type="file"
-            accept=".txt"
-            id="input-field"
-            className="input-field"
-            hidden
-            onChange={handleFileChange}
-          />
-          {file ? (
-            <img src={file} width={60} height={60} alt={fileName} />
-          ) : (
-            <div className="uploadingIcon">
-              <MdCloudUpload
+    <>
+      {!loading ?  (
+        <main>
+          <div className="fileUploadWrapper">
+            <form
+              onClick={() => document.querySelector(".input-field").click()}
+            >
+              <input
+                type="file"
+                accept=".txt"
+                id="input-field"
                 className="input-field"
-                color="#1475cf"
-                size={60}
+                hidden
+                onChange={handleFileChange}
               />
-              <p className="input-field">Choose Your Text File</p>
-            </div>
-          )}
-        </form>
-        <section className="uploaded-row">
-          <AiFillFileImage color="#1475cf" />
-          <span>
-            {fileName}
-            <MdDelete
-              className="deleteIcon"
-              onClick={() => {
-                setFileName("No Selected File");
-                setFile(null);
-              }}
-            />
-          </span>
-        </section>
-        {file && (
-          <div className="btnDiv">
-            <button className="btn" onClick={submitData}>
-              Submit
-            </button>
+              {file ? (
+                <img src={file} width={60} height={60} alt={fileName} />
+              ) : (
+                <div className="uploadingIcon">
+                  <MdCloudUpload
+                    className="input-field"
+                    color="#1475cf"
+                    size={60}
+                  />
+                  <p className="input-field">Choose Your Text File</p>
+                </div>
+              )}
+            </form>
+            <section className="uploaded-row">
+              <AiFillFileImage color="#1475cf" />
+              <span>
+                {fileName}
+                <MdDelete
+                  className="deleteIcon"
+                  onClick={() => {
+                    setFileName("No Selected File");
+                    setFile(null);
+                  }}
+                />
+              </span>
+            </section>
+            {file && (
+              <div className="btnDiv">
+                <button className="btn" onClick={submitData}>
+                  Submit
+                </button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </main>
+        </main>
+       ) : (
+        <DashBoard response={res}/>
+      )} 
+    </>
   );
 }
